5f3d0cb454f30a3dec8ec6def69cb9cb5c069da4
hamasfaa <hamasfa@gmail.com>
2025-02-15 23:29:15 +0700

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import TesView from '@/views/TesView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: TesView,
+    },
+  ],
+})
+
+export default router
